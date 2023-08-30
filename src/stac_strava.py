d3b968e2bce2347270b693a9ad563e5ca8b08fd5
Zachary Deziel <zachary.deziel@gmail.com>
2023-08-30 14:50:51 -0700

    Update datetime field of items and change location of collection.json file

--- src/stac_strava.py
+++ src/stac_strava.py
@@ -2,6 +2,7 @@ import os
 import json
 from datetime import datetime
 import pytz
+from pathlib import Path
 
 import pandas as pd
 from pystac import Collection, Extent, SpatialExtent, TemporalExtent
@@ -15,12 +16,11 @@ from utils import (
 
 
 def generate_stac_collection(
-    min_lat, min_lon, max_lat, max_lon, earliest_time, latest_time, collection_link
+    min_lat, min_lon, max_lat, max_lon, earliest_time, latest_time, collection_id
 ):
     earliest_time = pytz.utc.localize(earliest_time)
     latest_time = pytz.utc.localize(latest_time)
 
-    collection_id = "strava-activities"
     collection_title = "Strava Activities"
     collection_description = "This is a collection of Strava activities."
     collection_extent = Extent(
@@ -32,7 +32,7 @@ def generate_stac_collection(
         title=collection_title,
         description=collection_description,
         extent=collection_extent,
-        href=collection_link,
+        href=collection_id,
     )
     return collection
 
@@ -40,7 +40,7 @@ def generate_stac_collection(
 def activity_to_stac(
     file_path,
     link_file,
-    collection_link,
+    collection_id,
     catalog_link,
     path_to_items,
     activity_data,
@@ -56,18 +56,23 @@ def activity_to_stac(
         coordinates, min_lat, min_lon, max_lat, max_lon
     )
 
+    date_str = activity_data["Activity Date"]
+    date_obj = datetime.strptime(date_str, "%b %d, %Y, %I:%M:%S %p")
+    date_obj_utc = pytz.utc.localize(date_obj)
+    date_iso = date_obj_utc.isoformat()
+
     item_id = str(activity_data["Activity ID"])
     stac_item = {
         "id": item_id,
         "type": "Feature",
         "stac_version": "1.0.0-beta.2",
         "stac_extensions": [],
-        "collection": collection_link.split("/")[-1],
+        "collection": collection_id.split("/")[-1],
         "geometry": {"type": "LineString", "coordinates": coordinates},
         "properties": {
             "name": activity_data["Activity Name"],
             "type": activity_data["Activity Type"],
-            "date": activity_data["Activity Date"],
+            "datetime": date_iso,
             "distance": activity_data["Distance"],
         },
         "assets": {
@@ -81,7 +86,7 @@ def activity_to_stac(
         },
         "links": [
             {"rel": "self", "href": f"{path_to_items}{item_id}.json"},
-            {"rel": "collection", "href": collection_link},
+            {"rel": "collection", "href": collection_id},
             {"rel": "root", "href": catalog_link},
         ],
     }
@@ -89,7 +94,7 @@ def activity_to_stac(
 
 
 def activities_to_stac_catalog(
-    csv_path, activities_folder, destination_folder, collection_link, catalog_link
+    csv_path, activities_folder, destination_folder, collection_id, catalog_link
 ):
     ensure_directory_exists(destination_folder)
     activities_df = pd.read_csv(csv_path)
@@ -100,8 +105,6 @@ def activities_to_stac_catalog(
     latest_time = datetime.min
 
     for _, row in activities_df.iterrows():
-        if generated_files_count == 10:
-                break
         try:
             file_name = row["Filename"].split("/")[-1]
         except AttributeError:
@@ -123,7 +126,7 @@ def activities_to_stac_catalog(
             stac_item, min_lat, min_lon, max_lat, max_lon = activity_to_stac(
                 file_path,
                 file_path,
-                collection_link,
+                collection_id,
                 catalog_link,
                 destination_folder,
                 row,
@@ -148,9 +151,14 @@ def activities_to_stac_catalog(
             print(f"File {file_path} does not exist!")
 
     collection = generate_stac_collection(
-        min_lat, min_lon, max_lat, max_lon, earliest_time, latest_time, collection_link
+        min_lat, min_lon, max_lat, max_lon, earliest_time, latest_time, collection_id
     )
-    with open(collection_link, "w") as file:
+
+    
+    parent_path = Path(destination_folder).parent
+    collection_path = os.path.join(parent_path, f"{collection_id}.json")
+    print(collection_path)
+    with open(collection_path, "w+") as file:
         json.dump(collection.to_dict(), file)
 
     return generated_files_count
